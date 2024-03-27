e377af8cb0f74b40438220fab555bdab00f6cb28
virghiu <virghiu@gmail.com>
2024-03-27 08:59:17 -0400

    add nav links

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const router = createBrowserRouter([
         element: <Landing />,
       },
       {
-        path: '/news',
+        path: '/newsletter',
         element: <NewsLetter />,
       },
       {
