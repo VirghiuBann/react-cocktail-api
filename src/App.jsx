167b4232b70e1bd00d2cd0bceaaad3f4a1369312
virghiu <virghiu@gmail.com>
2024-03-27 11:25:36 -0400

    render cocktail info

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const router = createBrowserRouter([
         element: <NewsLetter />,
       },
       {
-        path: '/cocktail',
+        path: '/cocktail/:id',
         element: <Cocktail />,
       },
     ],
