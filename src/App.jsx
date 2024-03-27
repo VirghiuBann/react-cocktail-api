5fe5fce731732866b469d4bed04d2d614bf0d8fd
virghiu <virghiu@gmail.com>
2024-03-26 22:26:09 -0400

    Inital commit, react router pages

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import {
+  About,
+  HomeLayout,
+  Landing,
+  Error,
+  NewsLetter,
+  Cocktail,
+} from './pages'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+])
+const App = () => {
+  return <RouterProvider router={router} />
+}
+export default App
